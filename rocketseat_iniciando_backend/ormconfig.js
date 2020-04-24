21e27cb8ba152d1dbd43c39930cbc910a43d1c6b
Jay Benedicto <soamazing@gmail.com>
2020-04-23 22:23:27 -0300

    user

---
+++ rocketseat_iniciando_backend/ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "type": process.env.DB_TYPE,
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE,
+  "schema": process.env.DB_SCHEMA,
+  "entities": [
+    "./src/models/*.ts"
+  ],
+  "migrations": [
+    "./src/database/migrations/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
---
+++ rocketseat_iniciando_backend/src/services/CreateAppointmentService.ts
@@ -0,0 +1,33 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+export default class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentsRepository);
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+}
